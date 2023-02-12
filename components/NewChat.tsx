0d6fae1c7b1a9d9e0477f7b22565bf5eb51f05b2
lobsang norbu <50455820+lobnor2@users.noreply.github.com>
2023-02-12 21:04:47 +0530

    added feature to createNewChat button and navigation

--- components/NewChat.tsx
+++ components/NewChat.tsx
@@ -1,8 +1,23 @@
 import { PlusIcon } from "@heroicons/react/24/solid";
+import { addDoc, collection, serverTimestamp } from "firebase/firestore";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
 import React from "react";
+import { db } from "../firebase";
 
 const NewChat = () => {
-  const createNewChat = async () => {};
+  const router = useRouter();
+  const { data: session } = useSession();
+  const createNewChat = async () => {
+    const doc = await addDoc(
+      collection(db, "users", session?.user?.email!, "chats"),
+      {
+        userId: session?.user?.email!,
+        createdAt: serverTimestamp(),
+      }
+    );
+    router.push(`/chat/${doc.id}`);
+  };
   return (
     <div
       onClick={createNewChat}
