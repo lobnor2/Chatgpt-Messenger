e3bd846cc6ea1ca7521811c78e86c84595cc54e6
lobsang norbu <50455820+lobnor2@users.noreply.github.com>
2023-02-14 00:26:29 +0530

    done setting openai api

---
+++ lib/queryApi.ts
@@ -0,0 +1,3 @@
+import openai from "./chatgpt";
+
+const query = async (prompt: string, chatId: string, model: string) => {};
---
+++ pages/api/askQuestion.ts
@@ -0,0 +1,29 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { query } from "firebase/firestore";
+import type { NextApiRequest, NextApiResponse } from "next";
+
+//response type data
+type Data = {
+  answer: string;
+};
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) {
+  //getting it from chatinput
+  const { prompt, chatId, model, session } = req.body;
+
+  if (!prompt) {
+    res.status(400).json({ answer: "Please provide a prompt!" });
+    return;
+  }
+  if (!chatId) {
+    res.status(400).json({ answer: "Please provide a valid chat ID!" });
+    return;
+  }
+  //chatGPT query
+  const response = await query(prompt, chatId, model);
+
+  res.status(200).json({ name: "John Doe" });
+}
