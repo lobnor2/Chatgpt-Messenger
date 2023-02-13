99825dcc4e8d37c0a650e148173a4a215810ca1f
lobsang norbu <50455820+lobnor2@users.noreply.github.com>
2023-02-13 22:49:28 +0530

    done chatinput component

--- app/chat/[id]/page.tsx
+++ app/chat/[id]/page.tsx
@@ -1,5 +1,23 @@
-const ChatPage = () => {
-  return <div>page</div>;
+"use client";
+import Chat from "../../../components/Chat";
+import ChatInput from "../../../components/ChatInput";
+
+type Props = {
+  params: {
+    id: string;
+  };
+};
+
+const ChatPage = ({ params: { id } }: Props) => {
+  return (
+    <div className="flex flex-col h-screen overflow-hidden">
+      {/* chat  */}
+      <Chat chatId={id} />
+
+      {/* chat input */}
+      <ChatInput chatId={id} />
+    </div>
+  );
 };
 
 export default ChatPage;
