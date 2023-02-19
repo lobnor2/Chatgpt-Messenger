f36ddfdf11f91365ea083f9c84d4051c102f1fdc
lobsang norbu <50455820+lobnor2@users.noreply.github.com>
2023-02-19 22:53:01 +0530

    added react select

--- components/ModelSelection.tsx
+++ components/ModelSelection.tsx
@@ -8,16 +8,23 @@ const fetchModels = () => {
 
 const ModelSelection = () => {
   const { data: models, isLoading } = useSWR("models", fetchModels);
+  const { data: model, mutate: setModel } = useSWR("model", {
+    fallbackData: "text-davinci-002",
+  });
   return (
     <div>
       <Select
         className="mt-2"
+        options={models?.modelOptions}
         isSearchable
         isLoading={isLoading}
         menuPosition="fixed"
         classNames={{
           control: (state) => "bg-[#434654] border-[#434654]",
         }}
+        placeholder={model}
+        defaultValue={model}
+        onChange={(e) => setModel(e.value)}
       />
     </div>
   );
